9642761792df7a51047b828b91d37b30a9142057
KemuelGulane <kemueltunacao69@gmail.com>
2025-09-19 09:17:06 +0800

    Add files via upload

---
+++ chapter_3/DebugThree1.java
@@ -0,0 +1,20 @@
+// This class calculates a waitperson's tip
+// as 15% of the bill
+import java.util.Scanner;
+public class DebugThree1 {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+System.out.print("Enter the amount of your check >> ");
+double check1 = input.nextDouble();
+System.out.print("Enter the amount of your friend's check >> ");
+double check2 = input.nextDouble();
+calcTip(check1);
+calcTip(check2);
+}
+
+public static void calcTip(double bill) {
+final double RATE = 0.15;
+double tip = bill * RATE;
+System.out.println("The tip should be at least $" + tip);
+   }
+}
