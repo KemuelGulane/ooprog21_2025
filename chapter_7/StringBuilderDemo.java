fb21b627f93b8aa5144175c5d5c10703a51369c0
KemuelGulane <kemueltunacao69@gmail.com>
2025-11-26 23:13:59 +0800

    Create StringBuilderDemo.java

---
+++ chapter_7/StringBuilderDemo.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("(StringBuilder 1) nameString: ");
+        String userString1 = input.nextLine();
+
+        StringBuilder sb1 = new StringBuilder(userString1);
+
+        System.out.println("Capacity of nameString is: " + sb1.capacity());
+
+        System.out.print("\nEnter new length for nameString: ");
+        int newLength1 = input.nextInt();
+        input.nextLine(); 
+        sb1.setLength(newLength1);
+
+        System.out.print("\n(StringBuilder 2) addressString: ");
+        String userString2 = input.nextLine();
+
+        StringBuilder sb2 = new StringBuilder(userString2);
+
+        System.out.println("Capacity of addressString is: " + sb2.capacity());
+
+        System.out.print("\nEnter new length for addressString: ");
+        int newLength2 = input.nextInt();
+        sb2.setLength(newLength2);
+        
+        System.out.println("\n(Length set to " + newLength1 + " for StringBuilder 1) The name is " + sb1.toString());
+        System.out.println("(Length set to " + newLength2 + " for StringBuilder 2) The address is " + sb2.toString());
+
+    }
